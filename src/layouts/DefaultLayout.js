8623195c3623c31a039565833d6e0fc946c30b14
canhheo2002 <tramphattai9624@gmail.com>
2023-11-10 09:46:59 +0700

    fix Implement navbar footer

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -7,46 +7,51 @@ import FooterStyle from '../components/partials/backend/footerstyle/footerstyle'
 import Layout1Route from '../router/layout1-route'
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 
-const DefaultLayout = ({ children }) => {
-const backToTop = document.getElementById("back-to-top")
+const DefaultLayout = ({children}) => {
+    const backToTop = document.getElementById("back-to-top")
 // console.log(backToTop)
-if( backToTop !== null && backToTop !== undefined ) {
-  document.getElementById("back-to-top").classList.add('animated','fadeOut')
-  window.addEventListener('scroll', (e) => {
-  if (document.documentElement.scrollTop > 50) {
-    document.getElementById("back-to-top").classList.remove("fadeOut")
-    document.getElementById("back-to-top").classList.add("fadeIn")
-  }else {
-    document.getElementById("back-to-top").classList.remove("fadeIn")
-    document.getElementById("back-to-top").classList.add("fadeOut")
-  }
-})
+    if (backToTop !== null && backToTop !== undefined) {
+        document.getElementById("back-to-top").classList.add('animated', 'fadeOut')
+        window.addEventListener('scroll', (e) => {
+            if (document.documentElement.scrollTop > 50) {
+                document.getElementById("back-to-top").classList.remove("fadeOut")
+                document.getElementById("back-to-top").classList.add("fadeIn")
+            } else {
+                document.getElementById("back-to-top").classList.remove("fadeIn")
+                document.getElementById("back-to-top").classList.add("fadeOut")
+            }
+        })
 // scroll body to 0px on click
-document.querySelector('#top').addEventListener('click', (e) => {
-  e.preventDefault()
-  window.scrollTo({top: 0, behavior: 'smooth'});
-})
-}
-    return(
+        document.querySelector('#top').addEventListener('click', (e) => {
+            e.preventDefault()
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        })
+    }
+    return (
         <>
-             <div id="back-to-top">
-            <Link className="top"   to="#" id="top"> <i className="fa fa-angle-up"></i> </Link>
+            <HeaderStyle1/>
+            <div id="back-to-top">
+                <Link className="top" to="#" id="top"> <i className="fa fa-angle-up"></i> </Link>
             </div>
             <div className="wraper">
-                <HeaderStyle1 />
+
                 <div className="content-page" id="content-page">
-                    <TransitionGroup>
-                        <CSSTransition
-                            // key={location.key}
-                            classNames="fade"
-                            timeout={300}
-                        >
-                            {children}
-                        </CSSTransition>
-                    </TransitionGroup>
+                    {/*<TransitionGroup>*/}
+                    {/*    <CSSTransition*/}
+                    {/*        // key={location.key}*/}
+                    {/*        classNames="fade"*/}
+                    {/*        timeout={300}*/}
+                    {/*    >*/}
+                    {/*        {children}*/}
+                    {/*    </CSSTransition>*/}
+                    {/*</TransitionGroup>*/}
                 </div>
             </div>
-            <FooterStyle />
+            <br/>
+
+
+            <FooterStyle/>
+
         </>
     )
 }
